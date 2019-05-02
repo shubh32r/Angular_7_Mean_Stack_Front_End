910fda6396ad28bb53a3739a6df36419b6f1c83d
shubh257 <42402247+shubh257@users.noreply.github.com>
2019-05-02 11:17:03 +0530

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'create', component: CreateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
